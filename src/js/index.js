fc2fa71e90ff8ab03c2c10574eedba9001f9c23a
Eduardo Andrade <edu2andrade@gmail.com>
2023-03-22 09:55:26 +0000

    feat: counter finished

--- src/js/index.js
+++ src/js/index.js
@@ -9,4 +9,35 @@ import "../styles/index.css";
 import Home from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+// ReactDOM.render(<Home />, document.querySelector("#app"));
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+const counter = () => {
+
+  seconds++;
+
+  if(seconds > 59) {
+    minutes++;
+    seconds = 0;
+  }
+
+  if(minutes > 59) {
+    hours++
+    minutes = 0;
+  }
+
+  ReactDOM.render(
+    <Home 
+      seconds={seconds}
+      minutes={minutes}
+      hours={hours}
+    />,
+    document.querySelector("#app"))
+};
+  
+setInterval(() => {
+  counter()
+}, 1000)
